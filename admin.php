9d7373c0985b87c8d188f3e4374148d8c947bbcf
kn0lz <vrillon99@gmail.com>
2016-08-26 02:33:29 +0900

    admin.php, board part was filled.

--- admin.php
+++ admin.php
@@ -35,22 +35,31 @@ include_once "connect_db.php";
 	  if($_SESSION['id'] === "admin") { ?>
 		<div class="form_settings">
 			<h2>Board Management</h2>
+			<?php
+			if(isset($_POST['pst_del'])) {
+				if(!empty($_POST['pst'])) {
+					// $count = count($_POST['']);				
+					foreach($_POST['pst'] as $selected) {
+						$query  = "DELETE FROM board WHERE no='" . $selected . "'";
+						$db->query($query);
+					}
+				}
+			}
+			?>
 			<table style="width:100%; border-spacing:0;">
 			  <tr><th>ch</th><th style="width:60%">Title</th><th>Writer</th></tr>
 				<?php
-					if(isset($_POST['brd_del'])) {
-						if(!empty($_POST[''])) {
-							// $count = count($_POST['']);				
-							foreach($_POST[''] as $selected) {
-								$query  = "DELETE FROM user WHERE no='" . $selected . "'";
-								$db->query($query);
-							}
-						}
-					}
-				?>
-			  <!--<tr><td>Item 1</td><td>Description of Item 1</td></tr>-->
+				$query  = "SELECT * FROM board";
+				$result = $db->query($query);
+				while($row = $result->fetch_assoc()){ ?>
+					<tr>
+					<td><?php echo '<input style="width: 5%" type="checkbox" name="pst[]" value="' . $row['no'] . '">' ?></td>
+					<td><?php echo $row['title'] ?></td>
+					<td><?php echo $row['id'] ?></td>
+					</tr>
+			<?php } ?>
 			</table>
-			<p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="brd_del" value="delete" /></p>
+			<p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="pst_del" value="delete" /></p>
 		</div>
 		<form action="admin.php" method="post">
 			<div class="form_settings">
--- connect_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new mysqli('localhost', 'root', 'apmsetup', 'club');
-
-if($db->connect_error){
-    die('db connect error');
-}
-
-$db->set_charset('utf8');
-?>
