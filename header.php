c00e0c35581033bd5e02a29383f7b9bd100361a9
5HARK <blgf6190@gmail.com>
2016-08-26 00:54:05 +0900

    add header.php & footer.php

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+	<title></title>
+	<meta name="description" content="website description" />
+	<meta name="keywords" content="website keywords, website keywords" />
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
+    </head>
+
+    <body>
+	<div id="main">
+	    <div id="header">
+		<div id="logo">
+		    <div id="logo_text">
+			<!-- class="logo_colour", allows you to change the colour of the text -->
+			<h1><a href="index.html">club's<span class="logo_colour"> board</span></a></h1>
+		    </div>
+		</div>
+		<div id="menubar">
+		    <ul id="menu">
+			<!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+			<li><a href="index.php">Home</a></li>
+			<li><a href="login.php">Login</a></li>
+			<li><a href="board.php">Board</a></li>
+		    </ul>
+		</div>
+	    </div>
